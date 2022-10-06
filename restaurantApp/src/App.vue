0a71c74169dd51d40591ef81daa18ef5153541b7
gryhkn <girayhakan23@gmail.com>
2022-10-06 21:09:28 +0300

    Initial commit

---
+++ restaurantApp/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<h1> {{name}} </h1>
+<button @click="placeOrder">Place Order</button>
+</template>
+
+
+
+<script>
+export default {
+  setup() {
+    const name = "The Snazzy Burger";
+
+    const placeOrder = () => { alert("Your order has been places")}
+
+    return { name, placeOrder };
+  },
+
+
+}
+ 
+</script>
+
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
