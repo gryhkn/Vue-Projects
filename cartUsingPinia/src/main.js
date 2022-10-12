0c939fd207ce4d560b9f90062f86efe5c3d078ea
gryhkn <girayhakan23@gmail.com>
2022-10-12 14:40:10 +0300

    Stores added

--- cartUsingPinia/src/main.js
+++ cartUsingPinia/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { createPinia } from "pinia";
 
 // Icons and Styles
 import FontAwesomePlugin from "./plugins/FontAwesome";
@@ -10,8 +11,11 @@ import AppButton from "./components/AppButton.vue";
 import AppCountInput from "./components/AppCountInput.vue";
 import AppModalOverlay from "./components/AppModalOverlay.vue";
 
+const pinia = createPinia();
+
 // Init App
 createApp(App)
+  .use(pinia)
   .use(FontAwesomePlugin)
   .component("AppButton", AppButton)
   .component("AppCountInput", AppCountInput)
