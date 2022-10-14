a0b6cb5925a929babab6972f79071d255ba252a2
gryhkn <girayhakan23@gmail.com>
2022-10-14 09:26:08 +0300

    Defined post store

--- basicPinia/src/stores/counter.js
+++ basicPinia/src/stores/counter.js
@@ -1,12 +1,46 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+export const usePostStore = defineStore({
+  id: "post",
+  state: () => ({
+    posts: [],
+    post: null,
+    loading: false,
+    error: null,
+  }),
 
-  return { count, doubleCount, increment }
-})
+  getters: {
+    getPostsPerAuthor: (state) => {
+      return (authorId) =>
+        state.posts.filter((post) => post.userId == authorId);
+    },
+  },
+
+  actions: {
+    async fetchPosts() {
+      this.posts = [];
+      this.loading = true;
+      try {
+        this.posts = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        ).then((response) => response.json());
+      } catch (error) {
+        this.error = error;
+      } finally {
+        this.loading = false;
+      }
+    },
+
+    async fetchPost(id) {
+      this.post = null;
+      this.loading = true;
+      try {
+        this.post = fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+      } catch (error) {
+        this.error = error;
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+});
